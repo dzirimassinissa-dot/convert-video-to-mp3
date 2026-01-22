0c2320dd597c1b9c0d6c54118ac163d359c65a8b
massi1234 <dziri_massinissa@hotmail.fr>
2026-01-22 14:01:56 +0100

    Restore foundation.layout import to fix unresolved references

--- app/src/main/java/com/example/convertvideotomp3/MainActivity.kt
+++ app/src/main/java/com/example/convertvideotomp3/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
